e9c0ebdecdcc10cbb73306716363b20a9fa985ce
2200031995sanjay <2200031995@kluniversity.in>
2025-02-22 21:29:16 +0530

    commit
    Login and SignUp

---
+++ e-commerce/src/pages/Signup.jsx
@@ -0,0 +1,51 @@
+import {auth} from "../firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
+const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignup = async(e)=>{
+        e.preventDefault();
+        try{
+            await createUserWithEmailAndPassword(auth, email, password);
+            alert("Account created successfully");
+        }catch(error){
+            alert(error.message);
+        }
+    };
+
+    return(
+        <form
+        onSubmit={handleSignup}
+        className="p-6 bg-[#F0F7F4] text-[#042A2B] min-h-screen flex flex-col items-center justify-center"
+      >
+        <h1 className="text-2xl font-bold text-[#F06543] mb-4">Signup</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-80 p-2 border border-[#B3BFB8] bg-[#A2E3C4] rounded mb-2 outline-none"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-80 p-2 border border-[#B3BFB8] bg-[#A2E3C4] rounded mb-4 outline-none"
+          required
+        />
+        <button
+          type="submit"
+          className="w-80 p-2 bg-[#F06543] text-white rounded hover:bg-[#d95639] transition"
+        >
+          Signup
+        </button>
+      </form>
+    );
+};
+
+export default Signup;
