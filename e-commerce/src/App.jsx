7adc079f239a432721df91d7909defd4971b9852
2200031995sanjay <2200031995@kluniversity.in>
2025-02-22 21:46:08 +0530

    commit
    App.jsx finished

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,10 +1,36 @@
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
+   
+  const {user , isAdmin} = useAuth();
 
   return (
-    <>
-    </>
+    <div  className="bg-[#F0F7F4] text-[#042A2B] min-h-screen" >
+      <Router>
+        <CartProvider>
+          <Navbar />
+          <div className="container mx-auto p-6">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/cart" element={user?<Cart />:<Navigate to="/login" />}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="signup" element={<Signup/>}/>
+              <Route path="/admin" element={isAdmin?<AdminDashboard/>:<Navigate to="/"/>}/>
+            </Routes>
+
+          </div>
+        </CartProvider>
+      </Router>
+
+    </div>
   )
 }
 
